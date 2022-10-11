49737bf244bf9ca937b1f9fb4e10bdaafadcab7c
ruicatz <ruileote@microsoft.com>
2022-10-11 19:50:55 +0000

    fix append_pid_files - stop pid

--- betaXplatPerformanceTool_v4.0.sh
+++ betaXplatPerformanceTool_v4.0.sh
@@ -514,10 +514,10 @@ if [ -f /tmp/betaXplatPerformanceTool_start-$DATE_START.pid ]
 		sudo zip -g $PACKAGE_NAME betaXplatPerformanceTool_start-$DATE_START.pid
 fi
 
-if [ -f /tmp/betaXplatPerformanceTool_stop-$DATE_START.pid ]
+if [ -f /tmp/betaXplatPerformanceTool_stop-$DATE_STOP.pid ]
 	then 
-		cp /tmp/betaXplatPerformanceTool_stop-$DATE_START.pid .
-		sudo zip -g $PACKAGE_NAME betaXplatPerformanceTool_stop-$DATE_START.pid
+		cp /tmp/betaXplatPerformanceTool_stop-$DATE_STOP.pid .
+		sudo zip -g $PACKAGE_NAME betaXplatPerformanceTool_stop-$DATE_STOP.pid
 fi
 }
 
