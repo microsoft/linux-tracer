0c6c55a238cfaf4915a589d2fbae719e9e055a8d
ruicatz <ruileote@microsoft.com>
2022-10-28 23:14:14 +0000

    change date position in legend

--- betaXplatPerformanceTool_v4.1.sh
+++ betaXplatPerformanceTool_v4.1.sh
@@ -342,11 +342,12 @@ echo "set xlabel 'seconds'" >> $DIRNAME/cpu_plot.plt
 echo "set ylabel 'CPU %'" >> $DIRNAME/cpu_plot.plt
 echo "set key noenhanced" >> $DIRNAME/cpu_plot.plt
 echo "set key right top outside" >> $DIRNAME/cpu_plot.plt
-echo "set label '              --- System information ---' at graph 1, graph 0.72" >> $DIRNAME/cpu_plot.plt
-echo "set label '    OS: $OS' at graph 1, graph 0.65" >> $DIRNAME/cpu_plot.plt
-echo "set label '    Real Time Protection: $RTP_TEST' at graph 1, graph 0.60" >> $DIRNAME/cpu_plot.plt
-echo "set label '    Passive Mode: $PASSV_M_TEST' at graph 1, graph 0.55" >> $DIRNAME/cpu_plot.plt
-echo "set label '    Behavior Monitoring: $BM_TEST' at graph 1, graph 0.50" >> $DIRNAME/cpu_plot.plt
+echo "set label '       ------ System information ------' at graph 1, graph 0.75" >> $DIRNAME/cpu_plot.plt
+echo "set label '       $PLOT_DATE' at graph 1, graph 0.70" >> $DIRNAME/cpu_plot.plt
+echo "set label '    OS: $OS' at graph 1, graph 0.63" >> $DIRNAME/cpu_plot.plt
+echo "set label '    Real Time Protection: $RTP_TEST' at graph 1, graph 0.58" >> $DIRNAME/cpu_plot.plt
+echo "set label '    Passive Mode: $PASSV_M_TEST' at graph 1, graph 0.53" >> $DIRNAME/cpu_plot.plt
+echo "set label '    Behavior Monitoring: $BM_TEST' at graph 1, graph 0.48" >> $DIRNAME/cpu_plot.plt
 echo "plot 'graphs/1_$PID1_NAME.cpu.plt' with linespoints title '$PID1_NAME','graphs/2_$PID2_NAME.cpu.plt' with linespoints title '$PID2_NAME','graphs/3_$PID3_NAME.cpu.plt' with linespoints title '$PID3_NAME','graphs/4_$PID4_NAME.cpu.plt' with linespoints title '$PID4_NAME'" >> $DIRNAME/cpu_plot.plt
 
 # Create plot.mem.plt script
@@ -357,11 +358,12 @@ echo "set xlabel 'seconds'" >> $DIRNAME/mem_plot.plt
 echo "set ylabel 'Memory %'" >> $DIRNAME/mem_plot.plt
 echo "set key noenhanced" >> $DIRNAME/mem_plot.plt
 echo "set key right top outside" >> $DIRNAME/mem_plot.plt
-echo "set label '              --- System information ---' at graph 1, graph 0.72" >> $DIRNAME/mem_plot.plt
-echo "set label '    OS: $OS' at graph 1, graph 0.65" >> $DIRNAME/mem_plot.plt
-echo "set label '    Real Time Protection: $RTP_TEST' at graph 1, graph 0.60" >> $DIRNAME/mem_plot.plt
-echo "set label '    Passive Mode: $PASSV_M_TEST' at graph 1, graph 0.55" >> $DIRNAME/mem_plot.plt
-echo "set label '    Behavior Monitoring: $BM_TEST' at graph 1, graph 0.50" >> $DIRNAME/mem_plot.plt
+echo "set label '       ------ System information ------' at graph 1, graph 0.75" >> $DIRNAME/mem_plot.plt
+echo "set label '       $PLOT_DATE' at graph 1, graph 0.70" >> $DIRNAME/memplot.plt
+echo "set label '    OS: $OS' at graph 1, graph 0.63" >> $DIRNAME/mem_plot.plt
+echo "set label '    Real Time Protection: $RTP_TEST' at graph 1, graph 0.58" >> $DIRNAME/mem_plot.plt
+echo "set label '    Passive Mode: $PASSV_M_TEST' at graph 1, graph 0.53" >> $DIRNAME/mem_plot.plt
+echo "set label '    Behavior Monitoring: $BM_TEST' at graph 1, graph 0.48" >> $DIRNAME/mem_plot.plt
 echo "plot 'graphs/1_$PID1_NAME.mem.plt' with linespoints title '$PID1_NAME','graphs/2_$PID2_NAME.mem.plt' with linespoints title '$PID2_NAME', 'graphs/3_$PID3_NAME.mem.plt' with linespoints title '$PID3_NAME','graphs/4_$PID4_NAME.mem.plt' with linespoints title '$PID4_NAME'" >> $DIRNAME/mem_plot.plt
 }
 
